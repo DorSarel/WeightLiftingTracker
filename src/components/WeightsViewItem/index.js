01e1b7c5dce6de158da3e22e2ca83291883b6595
Dor Sarel <Dor.Sarel@wdc.com>
2020-03-21 12:12:42 +0200

    Add md breakpoint for WeightsView

--- src/components/WeightsViewItem/index.js
+++ src/components/WeightsViewItem/index.js
@@ -5,15 +5,17 @@ const WeightsViewItem = ({ title, currentWeight, lastWeight }) => {
     <div className='weight-view'>
       <div className='weight-view__content'>
         <h2 className='weight-view__title'>{title}</h2>
-        <div className='weight-view__data weight-view__data--current'>
-          <p className='weight-view__value'>{currentWeight}Kg</p>
-          <p>current</p>
-        </div>
-        <div className='weight-view__data'>
-          <p className='weight-view__value'>
-            {lastWeight === 0 ? 'N/A' : lastWeight}
-          </p>
-          <p>previous</p>
+        <div className='weight-view__weights'>
+          <div className='weight-view__data weight-view__data--current'>
+            <p className='weight-view__value'>{currentWeight}Kg</p>
+            <p>current</p>
+          </div>
+          <div className='weight-view__data'>
+            <p className='weight-view__value'>
+              {lastWeight === 0 ? 'N/A' : lastWeight}
+            </p>
+            <p>previous</p>
+          </div>
         </div>
       </div>
       <button className='btn'>View data</button>
