496faeddcef7ce745263776e4fad7d9414df3ca4
Dor Sarel <Dor.Sarel@wdc.com>
2020-04-18 02:35:10 +0300

    Calculate change in percentage for weight & body fat

--- src/components/UserInfoItem/index.js
+++ src/components/UserInfoItem/index.js
@@ -3,15 +3,29 @@ import Icons from '../../assets/sprite.svg';
 import PropTypes from 'prop-types';
 
 const UserInfoItem = ({ label, value, previous, unit = '' }) => {
+  let wrapperClass = 'user-info__change';
+  let iconTriangleDirection = 'up';
+  let changeInPercentage = 0;
+  if (!previous) {
+    wrapperClass += ' user-info__change--hidden';
+  } else {
+    if (previous > value) iconTriangleDirection = 'down';
+    changeInPercentage = Math.abs(
+      100 - (100 * parseFloat(value)) / parseFloat(previous)
+    );
+  }
+
   return (
     <div className='user-info__item'>
       <p className='user-info__value'>{`${value}${unit}`}</p>
       <p className='user-info__key'>{label}</p>
-      <div className='user-info__change'>
+      <div className={wrapperClass}>
         <svg className='user-info__icon'>
-          <use xlinkHref={`${Icons}#icon-triangle-up`} />
+          <use xlinkHref={`${Icons}#icon-triangle-${iconTriangleDirection}`} />
         </svg>
-        <p className='user-info__percentage'>0.5%</p>
+        <p className='user-info__percentage'>
+          {changeInPercentage.toFixed(2)}%
+        </p>
       </div>
     </div>
   );
