94f66e60537bb3505294426eafc7331faa7bdbc9
Dor Sarel <Dor.Sarel@wdc.com>
2020-04-18 02:16:35 +0300

    update weight & fat user data to include last value before change

--- src/components/UserInfoItem/index.js
+++ src/components/UserInfoItem/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Icons from '../../assets/sprite.svg';
 import PropTypes from 'prop-types';
 
-const UserInfoItem = ({ label, value, unit = '' }) => {
+const UserInfoItem = ({ label, value, previous, unit = '' }) => {
   return (
     <div className='user-info__item'>
       <p className='user-info__value'>{`${value}${unit}`}</p>
       <p className='user-info__key'>{label}</p>
-      <div className='user-info__change user-info__change--up'>
+      <div className='user-info__change'>
         <svg className='user-info__icon'>
           <use xlinkHref={`${Icons}#icon-triangle-up`} />
         </svg>
@@ -20,6 +20,7 @@ const UserInfoItem = ({ label, value, unit = '' }) => {
 UserInfoItem.propTypes = {
   label: PropTypes.string.isRequired,
   value: PropTypes.number.isRequired,
+  previous: PropTypes.any,
   unit: PropTypes.string,
 };
 
