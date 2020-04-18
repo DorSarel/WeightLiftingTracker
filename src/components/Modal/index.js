b7c2ec9e0248ff9f2fffbfb6be73fa2c17f261d4
Dor Sarel <Dor.Sarel@wdc.com>
2020-04-18 17:00:23 +0300

    Add close sign to Modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,7 +4,10 @@ import './style.scss';
 const Modal = () => (
   <div className='modal'>
     <div className='modal__box'>
-      <h3 className='modal__title'>Title</h3>
+      <div className='modal__header'>
+        <h3 className='modal__title'>Title</h3>
+        <span className='close'>&times;</span>
+      </div>
       <p className='modal__question'>Are you sure you want to logout?</p>
       <button className='modal__btn modal__btn--confirm'>Confirm</button>
       <button className='modal__btn modal__btn--cancel'>Cancel</button>
