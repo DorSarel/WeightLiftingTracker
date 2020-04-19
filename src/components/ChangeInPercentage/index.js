3a27074750af657e39fef63717d0f3e9a64a2b2e
Dor Sarel <Dor.Sarel@wdc.com>
2020-04-19 22:05:07 +0300

    Add ChangeInPercentage component to view change

---
+++ src/components/ChangeInPercentage/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Icons from '../../assets/sprite.svg';
+import './style.scss';
+
+const ChangeInPercentage = ({ current, previous }) => {
+  let wrapperClass = 'change';
+  let iconTriangleDirection = 'up';
+  let changeInPercentage = 0;
+  if (!previous) {
+    wrapperClass += ' change--hidden';
+  } else {
+    if (previous > current) iconTriangleDirection = 'down';
+    changeInPercentage = Math.abs(
+      100 - (100 * parseFloat(current)) / parseFloat(previous)
+    );
+  }
+
+  return (
+    <div className={wrapperClass}>
+      <svg className='change__icon'>
+        <use xlinkHref={`${Icons}#icon-triangle-${iconTriangleDirection}`} />
+      </svg>
+      <p className='change__percentage'>{changeInPercentage.toFixed(2)}%</p>
+    </div>
+  );
+};
+
+ChangeInPercentage.propTypes = {
+  current: PropTypes.number.isRequired,
+  previous: PropTypes.number.isRequired,
+};
+
+export default ChangeInPercentage;
