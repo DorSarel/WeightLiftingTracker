12dd73cfc28e7b9f04dda39baa26e7286561cdfc
Dor Sarel <Dor.Sarel@wdc.com>
2020-04-22 18:14:10 +0300

    transform userWeight from object to array and map it to WeightViewItem component

--- src/components/WeightsView/index.js
+++ src/components/WeightsView/index.js
@@ -5,18 +5,7 @@ import './style.scss';
 const WeightsView = ({ weights, revertWeight, removeWeight }) => {
   let userWeights = [];
   for (let key in weights) {
-    const { previous, current } = weights[key];
-    userWeights.push(
-      <WeightsViewItem
-        key={key}
-        title={key}
-        currentWeight={current}
-        lastWeight={previous}
-        onRevert={revertWeight}
-        onRemove={removeWeight}
-        disable={weights[key].exercisePeriodData.length < 2}
-      />
-    );
+    userWeights.push(weights[key]);
   }
 
   return (
@@ -24,7 +13,17 @@ const WeightsView = ({ weights, revertWeight, removeWeight }) => {
       <h1 className='heading-1'>My Weights</h1>
       <div className='weights'>
         {userWeights.length > 0 ? (
-          userWeights
+          userWeights.map((weightItem) => (
+            <WeightsViewItem
+              key={weightItem.exercise}
+              title={weightItem.exercise}
+              currentWeight={weightItem.current}
+              lastWeight={weightItem.previous}
+              onRevert={revertWeight}
+              onRemove={removeWeight}
+              disable={weightItem.exercisePeriodData.length < 2}
+            />
+          ))
         ) : (
           <p style={{ gridColumn: '1 / -1' }}>
             You didn't add any weights... Start adding now!
