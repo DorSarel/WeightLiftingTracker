53c264261b8058ccc12521b9e5992289ec9bd83e
Dor Sarel <Dor.Sarel@wdc.com>
2020-04-22 08:42:39 +0300

    Fix weight-view style for screens between 320px to 360px. Update modal-box offset on screen

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,42 +1,52 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import './style.scss';
 
-const Modal = ({
-  title,
-  actionText,
-  handleConfirm,
-  handleClose,
-  positionBoxClass = 'modal__box--top',
-}) => (
-  <div className='modal'>
-    <div className={`modal__box ${positionBoxClass}`}>
-      <div className='modal__header'>
-        <h3 className='modal__title'>{title}</h3>
-        <span onClick={handleClose} className='close'>
-          &times;
-        </span>
+const Modal = ({ title, actionText, handleConfirm, handleClose }) => {
+  const elementToScrollTo = useRef(null);
+
+  useEffect(() => {
+    const viewportOffsets = elementToScrollTo.current.getBoundingClientRect();
+    console.log(viewportOffsets);
+    window.scrollTo({
+      top: window.innerHeight / 2 + elementToScrollTo.current.offsetHeight,
+      left: viewportOffsets.left,
+      behavior: 'smooth',
+    });
+  }, []);
+
+  return (
+    <div className='modal'>
+      <div ref={elementToScrollTo} className='modal__box'>
+        <div className='modal__header'>
+          <h3 className='modal__title'>{title}</h3>
+          <span onClick={handleClose} className='close'>
+            &times;
+          </span>
+        </div>
+        <p className='modal__question'>
+          Are you sure you want to {actionText}?
+        </p>
+        <button
+          onClick={handleConfirm}
+          className='modal__btn modal__btn--confirm'
+        >
+          Confirm
+        </button>
+        <button onClick={handleClose} className='modal__btn modal__btn--cancel'>
+          Cancel
+        </button>
       </div>
-      <p className='modal__question'>Are you sure you want to {actionText}?</p>
-      <button
-        onClick={handleConfirm}
-        className='modal__btn modal__btn--confirm'
-      >
-        Confirm
-      </button>
-      <button onClick={handleClose} className='modal__btn modal__btn--cancel'>
-        Cancel
-      </button>
     </div>
-  </div>
-);
+  );
+};
 
 Modal.propTypes = {
   title: PropTypes.string.isRequired,
   actionText: PropTypes.string.isRequired,
   handleConfirm: PropTypes.func.isRequired,
   handleClose: PropTypes.func.isRequired,
-  styleBoxPosition: PropTypes.object,
+  positionBoxClass: PropTypes.string,
 };
 
 export default Modal;
